9eaf03d92bf3ac1807be18bfef31d52b35e6b738
xliang888 <xliang888@hotmail.com>
2020-01-21 16:48:52 -0500

    change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,11 +4,11 @@ pipeline {
    * Run everything on an existing agent configured with a label 'docker'.
    * This agent will need docker, git and a jdk installed at a minimum.
    */
- // agent {
+ agent {
  //   node {
  //     label 'docker'
  //   }
-//  }
+ }
 
   // using the Timestamper plugin we can add timestamps to the console log
   options {
