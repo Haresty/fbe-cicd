ba1ee4d5110e7c3c0470f35f8bf240fc6ac7c19a
xliang888 <xliang888@hotmail.com>
2020-01-21 20:35:58 -0500

    new Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,51 +1,5 @@
+#!/usr/bin/env groovy
 
-pipeline {
+@Library("pipeline-shared-common@master") _shared_common_lib
 
- agent {
-    node {
-      label 'master'
-    }
- }
-
-  stages {
-    stage('Build') {
-      steps {
-		echo 'Run Build ...'
-      }
-    }
-
-    stage('Quality Analysis') {
-      parallel {
-        // run Sonar Scan and Integration tests in parallel. This syntax requires Declarative Pipeline 1.2 or higher
-        stage ('Integration Test') {
-          agent any  //run this stage on any available agent
-          steps {
-            echo 'Run integration tests here...'
-          }
-        }
-        stage('Sonar Scan') {
-          agent any
-          steps {
-            echo 'Run Sonar Scan tests here...'
-          }
-        }
-      }
-    }
-
-    stage('Build and Publish Image') {
-
-      steps {
-		echo 'Run Build and Publish Image here...'
-      }
-    }
-  }
-
-  post {
-    failure {
-      // notify users when the Pipeline fails
-      mail to: 'xliang888@hotmail.com',
-          subject: "Failed Pipeline",
-          body: "Something is wrong"
-    }
-  }
-}
+tngFbeCicdPipeline()
