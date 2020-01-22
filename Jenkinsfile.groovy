58fd68500a1c3bb5726ba25bd799e420e6de25bf
xliang888 <xliang888@hotmail.com>
2020-01-21 21:20:11 -0500

    new change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
 
-@Library("github.com/Haresty/pipeline-shared-common@master") _shared_common_lib
+@Library("github.com/Haresty/pipeline-shared-common@master")
 tngFbeCicdPipeline()
